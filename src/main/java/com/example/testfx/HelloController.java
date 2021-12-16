170087b99faaa1229c3fc2f999bf111c7eec77da
miquellopez97 <58914599+miquellopez97@users.noreply.github.com>
2021-12-16 15:33:18 +0100

    jeje

--- src/main/java/com/example/testfx/HelloController.java
+++ src/main/java/com/example/testfx/HelloController.java
@@ -4,6 +4,10 @@ package com.example.testfx;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
@@ -11,10 +15,25 @@ import javafx.scene.shape.Circle;
 public class HelloController {
 
     @FXML
-    Circle circle = new Circle();
+    BorderPane background;
 
     @FXML
-    public void changeBG(Event e){
-        circle.setFill(Color.RED);
+    public void changeRed(Event e){
+        background.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, null)));
+    }
+
+    @FXML
+    public void changeGreen(Event e){
+        background.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, null)));
+    }
+
+    @FXML
+    public void changeYellow(Event e){
+        background.setBackground(new Background(new BackgroundFill(Color.YELLOW, CornerRadii.EMPTY, null)));
+    }
+
+    @FXML
+    public void changeBlue(Event e){
+        background.setBackground(new Background(new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, null)));
     }
 }
